40376931353b757b20315be7c3537ba1d1af3b97
Joshua Tobi Ajagbe <65833013+Joepolymath@users.noreply.github.com>
2024-11-25 18:02:27 +0100

    learnt the "using" keyword

--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 
 double calculateCircleCircumference(double);
 
+// typedef std::string text_t; // either this 
+using text_t = std::string; // or this...
+
 int main() {
 	double radius = 3.2;
 	double circumference = calculateCircleCircumference(radius);
 	std::cout << circumference << std::endl;
+
+	text_t name = "Joshua";
 	return 0;
 }
 
