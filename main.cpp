8a0cfb2cec7cd3346a26563984560195c4c9739b
Joshua Tobi Ajagbe <65833013+Joepolymath@users.noreply.github.com>
2024-12-02 10:14:48 +0100

    getRandom

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 double calculateCircleCircumference(double);
 double calculateCircleArea(double);
+int getRandom(int);
 
 // typedef std::string text_t; // either this 
 using text_t = std::string; // or this...
@@ -26,6 +27,7 @@ int main() {
 	double circumference = calculateCircleCircumference(radius);
 	double area = calculateCircleArea(radius);
 
+
 	std::cout << "Circumference is " << circumference << std::endl;
 	std::cout << "Area " << area << std::endl;
 
@@ -36,6 +38,8 @@ int main() {
 
 	double hypothenus = calcHypothenus(opposite, adjacent);
 	std::cout << "Hypothenus is " << hypothenus << std::endl;
+
+	std::cout << "This is a random integer. " << getRandom(10) << '\n';
 
 	return 0;
 }
@@ -48,4 +52,10 @@ double calculateCircleCircumference(double radius) {
 double calculateCircleArea(double radius) {
 	const double PI = 3.14159;
 	return PI * (pow(radius, 2));
+}
+
+int getRandom(int max) {
+	srand(time(NULL));
+
+	return rand() % (max + 1);
 }
